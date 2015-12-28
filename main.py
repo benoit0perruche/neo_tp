e0f6fc2f14b53df8b14774b3a87811d8d8f30260
augustin <augustin.lafanechere@gmail.com>
2015-12-28 01:31:40 +0100

    test node creation from csv file

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-from py2neo import Graph, Path
+from parsers import DjsParser
+from py2neo import authenticate, Graph, Path
 
-graph = Graph()
+if __name__ == '__main__':
 
+    authenticate("127.0.0.1:7474", "neo4j", "neotp")
+    graph = Graph("http://127.0.0.1:7474/db/data/")
+    tx = graph.cypher.begin()
 
-
-tx = graph.cypher.begin()
-
-for name in ["Alice", "Bob", "Carol"]:
-
-    tx.append("CREATE (person:Person {name:{name}}) RETURN person", name=name)
-
-alice, bob, carol = [result.one for result in tx.commit()]
-
-
-
-friends = Path(alice, "KNOWS", bob, "KNOWS", carol)
-
-graph.create(friends)
+    djs_parser = DjsParser("raw_data/djs.csv")
+    for dj in djs_parser.read_csv():
+        tx.append("CREATE (dj:Dj {dj_id:{dj_id}, name:{name}, soundcloud_url:{soundcloud_url}}) RETURN dj",
+                  dj_id=dj.dj_id,
+                  name=dj.name,
+                  soundcloud_url=dj.soundcloud_url)
+    tx.commit()
