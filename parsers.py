ac0ac8df764d5b0330cec777d398191e4ec7f99e
augustin <augustin.lafanechere@gmail.com>
2015-12-31 12:04:43 +0100

    create new parsers

--- parsers.py
+++ parsers.py
@@ -10,7 +10,7 @@ class DjsParser():
     def get_djs_countries(self):
         with open(self.csv_filepath) as csv_file:
             reader = csv.DictReader(csv_file, delimiter=",")
-            return [(Node("Dj", dj_id=row['dj_id'], name=row['name'], souncloud_url=row['soundcloud_url']), row['country']) for row in reader]
+            return [(Node("Dj", dj_id=row['dj_id'], name=row['name'], souncloud_url=row['soundcloud_url'], soundcloud_id=row['soundcloud_id']), row['country']) for row in reader]
 
 
 class ClubsParser():
@@ -32,7 +32,8 @@ class EventsParser():
     def get_events_clubs(self):
         with open(self.csv_filepath) as csv_file:
             reader = csv.DictReader(csv_file, delimiter=",")
-            return [(Node("Event", event_id=row["event_id"], date=row['date']), row["club_id"])for row in reader]
+            return [(Node("Event", event_id=row["event_id"]), row["club_id"], row['date']) for row in reader]
+
 
 class DjsEventsParser():
 
@@ -43,3 +44,48 @@ class DjsEventsParser():
         with open(self.csv_filepath) as csv_file:
             reader = csv.DictReader(csv_file, delimiter=",")
             return [(row['dj_id'], row["event_id"])for row in reader]
+
+
+class TrackParser():
+
+    def __init__(self, csv_filepath):
+        self.csv_filepath = csv_filepath
+
+    def get_events_clubs(self):
+        with open(self.csv_filepath) as csv_file:
+            reader = csv.DictReader(csv_file, delimiter=",")
+            return [(Node("Track",
+                          title=row["title"],
+                          duration=row['duration'],
+                          playback_count=row['playback_count'],
+                          permalink=row['permalink'],
+                          ), row["dj_id"], row['release_date'])for row in reader]
+
+
+class BoilerRoomParser():
+
+    def __init__(self, csv_filepath):
+        self.csv_filepath = csv_filepath
+
+    def get_boiler_djs(self):
+        with open(self.csv_filepath) as csv_file:
+            reader = csv.DictReader(csv_file, delimiter=",")
+            return [(Node("BoilerRoom",
+                          id=row['br_id'],
+                          title=row["title"],
+                          view_count=row['view_count'],
+                          like_count=row['like_count'],
+                          permalink='http://www.youtube.com/watch?v='+row['br_id'],
+                          ), row["dj_id"], row['published_date'])for row in reader]
+
+
+class TopParser():
+
+    def __init__(self, csv_filepath):
+        self.csv_filepath = csv_filepath
+
+    def get_top(self):
+        with open(self.csv_filepath) as csv_file:
+            reader = csv.DictReader(csv_file, delimiter=",")
+
+            return [(row['rank'][:-1], row['dj_name'].lower()) for row in reader]
